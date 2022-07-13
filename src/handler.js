146ddbe1e885ce61b9e3f91bfeb47263f168e6cf
hadi <hadipsy27@gmail.com>
2022-07-13 21:46:53 +0700

    Get book by Id

--- src/handler.js
+++ src/handler.js
@@ -85,4 +85,31 @@ const getAllBookHandler = () => ({
   },
 });
 
-module.exports = { addBookHandler, getAllBookHandler };
+// eslint-disable-next-line consistent-return
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.filter((n) => n.id === id)[0];
+
+  if (book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = {
+  addBookHandler,
+  getAllBookHandler,
+  getBookByIdHandler,
+};
